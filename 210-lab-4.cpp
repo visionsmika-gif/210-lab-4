b1c5659f3253769dca08b24d68613aafb294fd2f
prof-deets <mark.dietrich@gmail.com>
2025-08-25 01:59:53 -0400

    Initial commit

---
+++ 210-lab-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // complete this code in order to make it output
+    // the numbers 1-100
+
+    for ()
+        cout << i << " ";
+
+    return 0;
+}
